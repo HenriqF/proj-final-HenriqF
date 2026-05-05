84b32dbafd59d5c08641f4e053908a9d0d752df2
Henrique Reinaldi <carlosofalmeida@gmail.com>
2026-05-05 14:09:07 -0300

    interface para sudoku

--- src/frontend/client.js
+++ src/frontend/client.js
@@ -1,25 +1,31 @@
 let sock;
-function ligar(){
-    sock = new WebSocket("ws://localhost:5015/ws"); 
+sock = new WebSocket("ws://127.0.0.1:5015/ws"); 
 
-    sock.onopen = () => {
-        input.value = "conectado"
-    }; 
+sock.onopen = () => {
+    input.value = "conectado"
+}; 
 
-    sock.onmessage = (event) => {
-        resposta.textContent = event.data.toString();
-    };
+sock.onmessage = (event) => {
+    let message = event.data.toString();
 
-    sock.onerror = (erro) => {
-        console.error("erro");
-    };
+    if(message.startsWith("sudoku:")){
+        jogando(message);
+    }
 
-    sock.onclose = () => {
-        setTimeout(ligar, 200);
-    };
-}
-ligar();
+    if(message.startsWith("ganhou:")||message.startsWith("perdeu:")){
+        document.getElementById("sus").style.display="none";
+    }
 
+    resposta.textContent = message;
+};
+
+sock.onerror = (erro) => {
+    console.error("erro");
+};
+
+sock.onclose = () => {
+    setTimeout(ligar, 200);
+};
 
 
 const botao = document.getElementById("butao");
@@ -29,3 +35,26 @@ const resposta = document.getElementById("resposta");
 botao.addEventListener("click", () => {
     sock.send(input.value);
 });
+
+function jogando(sudoku){
+    document.getElementById("sus").style.display = "block";
+
+    for(let i=1,  j=7; j<sudoku.length;i++, j++){
+        if(sudoku[j]!='_'){
+            document.getElementById(`${i}`).value = sudoku[j]
+            document.getElementById(`${i}`).disabled = true
+        }
+        else{
+            document.getElementById(`${i}`).value = ""
+            document.getElementById(`${i}`).disabled = false
+        }
+    }
+}
+
+function finalizar(){
+    let out = ""
+    for(let i=1; i<37;i++){
+        out += `${document.getElementById(`${i}`).value}`
+    }
+    sock.send(out)
+}
